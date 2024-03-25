76e8be3368b8fc43e72543109bc4ee16a3a38184
htetahyan <htetahyan@gmail.com>
2024-03-25 03:11:31 -0400

    refactor: profile auth

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const config = {
     host: process.env.DB_HOST ,
     database: process.env.DB_NAME,
-    port:  3306,
+    port:  process.env.DB_PORT,
     user: process.env.DB_USER ,
     password: process.env.DB_PASSWORD ,
     waitForConnections: true,
