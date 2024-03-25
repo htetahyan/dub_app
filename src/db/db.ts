8c3cc29ace719ac4f63b9d298ae4be784865de3b
htetahyan <htetahyan@gmail.com>
2024-03-25 03:12:37 -0400

    refactor: change db

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const config = {
     host: process.env.DB_HOST ,
     database: process.env.DB_NAME,
-    port:  process.env.DB_PORT,
+    port:  4000,
     user: process.env.DB_USER ,
     password: process.env.DB_PASSWORD ,
     waitForConnections: true,
